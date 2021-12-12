fefebf2bb868be8da7456e7e7d43bb8c5042f84f
LucasG <lgvivanco96@gmail.com>
2021-12-12 15:14:51 -0300

    Added a script for modify GOES16 images, removing NOAA logo and moving the datetime on the image to the upper left corner.

--- changebackground_GOES16.sh
+++ changebackground_GOES16.sh
@@ -1,12 +1,13 @@
-#bin/bash
+#!/usr/bin/bash
 
 #Created on Sat Nov 20 14:51:06 2021
 #@author: lucas
-# =================================================================================================
+# =============================================================================
 # Script for changing gnome desktop background to the latest full disk GOES16 
 # image. 
-# =================================================================================================
+# =============================================================================
 
+echo Current datetime: $(date)
 # Get the Real Username
 RUID=$(who | awk 'FNR == 1 {print $1}')
 
@@ -18,28 +19,34 @@ imgdir=/home/lucas/programs/LOOP_BACKGROUND/images
 
 #Download latest image
 echo Downloading if there is any new GOES16 image...
-sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" $imgdir/.././request_GOES16.py $imgdir > .request
+$imgdir/.././request_GOES16.py $imgdir > .request
+#sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" $imgdir/.././request_GOES16.py $imgdir > .request
+
 code=$(cat .request | grep code | cut -c20-)
-if test -f "./.CURRENTBG_$code.jpg"; then
+cond=$(tail -n 1 .request)
+
+if [[ "$cond" == "Latest image already exist." ]]; then
     echo There aint new images!
     echo Doing nothing instead.
     echo Bye!
+    exit
 else
     filename=$(ls -t $imgdir/GOES16_GEOCOLOR_* | head -n 1)
     filename=${filename#$imgdir}
-    echo Done with download.
-
+    echo Editing GOES16 image.
+    $imgdir/.././removelogo_GOES16.py $imgdir/$filename > .removelogo
     #Set latest downloaded image to desktop background
     echo Setting "'file://${imgdir}/${filename}'" as desktop background
     sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/primary-color" "'#000000'"
     sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/picture-options" "'scaled'"
     sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/picture-uri" "'file://$imgdir/$filename'"
-    cp $imgdir/$filename $imgdir/../$filename
-    mv $imgdir/../$filename $imgdir/../.CURRENTBG_$code.jpg
+
+    
 fi
 #Remove images
 if [ $(ls $imgdir | grep GOES16_GEOCOLOR -c) -gt 6 ]; then
     list=$(ls -t $imgdir/GOES16_GEOCOLOR_* | tail -n5)
     rm -rf $list
 fi
+
 exit
--- request_GOES16.py
+++ request_GOES16.py
@@ -13,24 +13,23 @@ Created on Sat Nov 20 14:51:06 2021
 import requests
 import os
 import sys
-from glob import glob
 from time import sleep
 
-print("Beggining request_GOES16.py...")
-#%%
-#Path to NOAA GOES16 Images Repository
+print("Beggining request_GOES16.py ...")
+# %%
+# Path to NOAA GOES16 Images Repository
 NOAA_GOES16 = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/FD/GEOCOLOR/"
 
 
-#Image resolution in pixels.
-#Aviable options: 339x339, 678x678, 1808x1808, 5424x5424, 10848x10848
-res         = "1808x1808"
+# Image resolution in pixels.
+# Aviable options: 339x339, 678x678, 1808x1808, 5424x5424, 10848x10848
+res = "1808x1808"
 print("Image resolution is: "+res+" pixels.")
 ###############################################################################
-#%%
-#Make HTTP request to NOAA repository
+# %%
+# Make HTTP request to NOAA repository
 req = ''
-while req =='':
+while req == '':
     try:
         req = requests.get(NOAA_GOES16)
         break
@@ -40,13 +39,13 @@ while req =='':
         sleep(10)
         continue
 print("Successfuly connecting to server...")
-#Save repository tree in a temporary file
-with open(".GOES16_DIRECTORY.txt","w") as file:
-	file.write(req.text)
-	file.close()
+# Save repository tree in a temporary file
+with open(".GOES16_DIRECTORY.txt", "w") as file:
+    file.write(req.text)
+    file.close()
 
-#Use regular expression bash commands to grab latest image url
-order = r'cat .GOES16_DIRECTORY.txt | \
+# Use regular expression bash commands to grab latest image url
+order = r'LANG=en_us_88591;cat .GOES16_DIRECTORY.txt | \
 	    grep '+res+r' | \
 	    grep $(date -u +%d-%b-%Y | sed -e "s/\b\(.\)/\u\1/g") | \
 	    tail -n 1'
@@ -55,23 +54,21 @@ url = url.split(">")[1]
 url = url.split("<")[0]
 url = NOAA_GOES16 + url
 
-#%%
-#Clean directory
+# %%
+# Clean directory
 os.system("rm -rf .GOES16_DIRECTORY.txt")
 
 
-img_code    = url.split("/")[-1].split("_")[0]
-#Path to downloaded image:
-# outpath     = "./images"  
-outpath     = sys.argv[1]
-outpath     = outpath+"/GOES16_GEOCOLOR_"+img_code+".jpg"
+img_code = url.split("/")[-1].split("_")[0]
+# Path to downloaded image:
+# outpath     = "./images"
+outpath = sys.argv[1]
+outpath = outpath+"/GOES16_GEOCOLOR_"+img_code+".jpg"
 
-#Download image in desired path if doesnt already exists.
+# Download image in desired path if doesnt already exists.
 print("Latest image code: "+img_code)
 if not os.path.isfile(outpath):
     os.system("wget -O "+outpath+" "+url)
     print("Done with download.")
 else:
     print("Latest image already exist.")
-
- 
