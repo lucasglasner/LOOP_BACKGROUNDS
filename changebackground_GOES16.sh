13c086cc9e1a15a11863e5c93f70943a1f9119dc
LucasG <lgvivanco96@gmail.com>
2022-07-21 09:19:55 -0400

    usando gsettings ahora con ubuntu 22.04

--- changebackground_GOES16.sh
+++ changebackground_GOES16.sh
@@ -8,6 +8,8 @@
 # =============================================================================
 
 echo Current datetime: $(date)
+#PID=$(pgrep gnome-session)
+#export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 # Get the Real Username
 RUID=$(who | awk 'FNR == 1 {print $1}')
 
@@ -16,7 +18,7 @@ RUSER_UID=$(id -u ${RUID})
 
 #Directory where to store downloaded images
 imgdir=/home/lucas/programs/LOOP_BACKGROUND/images
-
+gsettings set org.gnome.desktop.background picture-options "scaled"
 #Download latest image
 echo Downloading if there is any new GOES16 image...
 $imgdir/.././request_GOES16.py $imgdir > .request
@@ -27,8 +29,11 @@ cond=$(tail -n 1 .request)
 
 if [[ "$cond" == "Latest image already exist." ]]; then
     echo There aint new images!
-    echo Doing nothing instead.
-    echo Bye!
+    echo Using last aviable image!
+    filename=$(ls -t $imgdir/GOES16_GEOCOLOR_* | head -n 1)
+    filename=${filename#$imgdir}
+    echo Setting "file://${imgdir}${filename}" as desktop background
+    gsettings set org.gnome.desktop.background picture-uri-dark file:///${imgdir}${filename}
     exit
 else
     filename=$(ls -t $imgdir/GOES16_GEOCOLOR_* | head -n 1)
@@ -37,9 +42,10 @@ else
     $imgdir/.././removelogo_GOES16.py $imgdir/$filename > .removelogo
     #Set latest downloaded image to desktop background
     echo Setting "'file://${imgdir}/${filename}'" as desktop background
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/primary-color" "'#000000'"
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/picture-options" "'scaled'"
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/picture-uri" "'file://$imgdir/$filename'"
+    gsettings set org.gnome.desktop.background picture-uri-dark file:///$imgdir/$filename
+    #sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/primary-color" "'#000000'"
+    #sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/picture-options" "'scaled'"
+    #sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" dconf write "/org/gnome/desktop/background/picture-uri" "'file://$imgdir/$filename'"
 
     
 fi
